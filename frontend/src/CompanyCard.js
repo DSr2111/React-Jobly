e0c81aec450c35d9544fef6941a43c6daa8b06ba
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-11-02 15:34:56 -0400

    card parts

--- frontend/src/CompanyCard.js
+++ frontend/src/CompanyCard.js
@@ -1,10 +1,18 @@
 import { React, useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
 
 function CompanyCard() {
-    return (
-
-    )
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <CardTitle></CardTitle>
+          <CardText></CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
 
 export default CompanyCard;
