804f5bef83e1b4152679c729f28cb99d56b50136
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-11-02 12:41:02 -0400

    added profile

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -5,6 +5,7 @@ import SignupForm from './SignupForm';
 import LoginForm from './LoginForm';
 import CompanyList from './CompanyList';
 import CompanyDetail from './CompanyDetail';
+import Profile from './Profile';
 
 function AppRoutes() {
   return (
@@ -15,6 +16,7 @@ function AppRoutes() {
       <Route path="/jobs" element={<Jobs />} />
       <Route path="/signup" element={<SignupForm />} />
       <Route path="/login" element={<LoginForm />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
