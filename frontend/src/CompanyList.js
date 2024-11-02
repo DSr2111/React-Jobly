be62eca91a92aba7b2c8873707d1ac45e358b003
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-11-02 17:35:07 -0400

    adding search bar to company list

--- frontend/src/CompanyList.js
+++ frontend/src/CompanyList.js
@@ -1,11 +1,21 @@
 import { React, useContext, useState } from 'react';
 import { JoblyApi } from '../api.js';
 import { Container, Row, Col } from 'reactstrap';
+import SearchBar from './SearchBar.js';
 
 function CompanyList() {
+  const [companies, setCompanies] = useState(null);
+
+  async function getCompanies(name) {
+    let companies = await JoblyApi.getCompanies(name);
+    setCompanies(companies);
+  }
+
   return (
     <div>
-      <Container></Container>
+      <Container>
+        <SearchBar filter={getCompanies} />
+      </Container>
     </div>
   );
 }
