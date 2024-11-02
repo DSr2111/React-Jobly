0a69eb95b5150e1a9be725cb193a4699a7b5d7b2
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-11-02 14:26:23 -0400

    app and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import { React, useState, useEffect } from 'react';
+import NavBar from './NavBar';
+import AppRoutes from './Routes';
 
 function App() {
   return (
-    
-  )
+    <div className="App">
+      <NavBar />
+      <AppRoutes />
+    </div>
+  );
 }
 
 export default App;
