74d6705b0c8a2515653ef4ae4410e66dae5a6ac6
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-11-02 12:16:39 -0400

    routes start

---
+++ frontend/src/Routes.js
@@ -0,0 +1,11 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+function Routes() {
+  return (
+    <Routes>
+      <Route path="/" />
+      <Route path="/" />
+      <Route path="/" />
+    </Routes>
+  );
+}
