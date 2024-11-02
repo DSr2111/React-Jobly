0aa720ce12c14794a9a525349563c7f3ce048a8d
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-11-02 16:48:30 -0400

    job card components

--- frontend/src/JobCard.js
+++ frontend/src/JobCard.js
@@ -1,6 +1,30 @@
 import { React, useContext, useState } from 'react';
 import Context from './Context';
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  ListGroup,
+  ListGroupItem,
+  Button,
+} from 'reactstrap';
 
 function JobCard() {
-  return <div></div>;
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <CardTitle></CardTitle>
+          <CardSubtitle></CardSubtitle>
+          <ListGroup>
+            <ListGroupItem></ListGroupItem>
+            <ListGroupItem></ListGroupItem>
+          </ListGroup>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
+
+export default JobCard;
