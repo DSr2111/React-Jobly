3ef7297509f7ed4dcd97bbdee17ae15dae9a3360
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-11-02 15:44:00 -0400

    simple company card

--- frontend/src/CompanyCard.js
+++ frontend/src/CompanyCard.js
@@ -2,13 +2,13 @@ import { React, useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
 
-function CompanyCard() {
+function CompanyCard({ company }) {
   return (
     <div>
-      <Card>
-        <CardBody>
-          <CardTitle></CardTitle>
-          <CardText></CardText>
+      <Card className="m-2">
+        <CardBody className="text-center">
+          <CardTitle tag="h3">{company.name}</CardTitle>
+          <CardText>{company.description}</CardText>
         </CardBody>
       </Card>
     </div>
